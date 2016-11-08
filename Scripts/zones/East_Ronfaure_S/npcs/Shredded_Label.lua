757f615da71f90d247d8d23e62320147cf6a86d7
Mizzguy <Mizzguy@users.noreply.github.com>
2016-11-08 18:35:08 -0500

    Update Shredded_Label.lua

--- Scripts/zones/East_Ronfaure_S/npcs/Shredded_Label.lua
+++ Scripts/zones/East_Ronfaure_S/npcs/Shredded_Label.lua
@@ -16,23 +16,35 @@ require("scripts/zones/West_Sarutabaruta_[S]/TextIDs");
 -----------------------------------
 
 function onTrigger(player,npc)
-          
-    if (player:hasKeyItem(RED_LABELED_CRATE) == true) then
-        SpawnMob(17109368):updateClaim(player);
-        for i = 17109369, 17109374, 1 do
-            SpawnMob(i):updateClaim(player);
-            local party = player:getParty();
-	    
-	        if (party ~= nil) then
-	            for i,v in ipairs(party) do
-                    v:addStatusEffect(EFFECT_LEVEL_RESTRICTION, 60, 0, 0);
-                end
-            end    
-        end           
-    else
-        player:PrintToPlayer("You do not possess the Key Item");    
-    end    
-end;
+    local party = player:getParty();
+    local alliance = player:getAlliance();
+    
+    if (alliance ~= nil) then
+         for i,v in ipairs(alliance) do
+             if (v:hasKeyItem(RED_LABELED_CRATE) == true) then
+                 v:addStatusEffect(EFFECT_LEVEL_RESTRICTION, 60, 0, 0);
+                 v:delKeyItem(RED_LABELED_CRATE);
+                 for i = 17109368, 17109374, 1 do
+                     SpawnMob(i):updateClaim(player);
+                 end
+             else
+                player:PrintToPlayer("You do not posses the Key Item");
+             end   
+         end
+     elseif (party ~= nil) then
+          for i,v in ipairs(party) do
+              if (v:hasKeyItem(RED_LABELED_CRATE) == true) then
+                  v:addStatusEffect(EFFECT_LEVEL_RESTRICTION, 60, 0, 0);
+                  v:delKeyItem(RED_LABELED_CRATE);
+                  for i = 17109368, 17109374, 1 do
+                      SpawnMob(i):updateClaim(player);
+                  end  
+              else
+                 player:PrintToPlayer("You do not posses the Key Item");
+              end
+         end
+    end
+end;        
 
 -----------------------------------
 -- onEventUpdate
