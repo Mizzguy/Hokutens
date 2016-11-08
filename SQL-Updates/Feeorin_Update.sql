b6c5e01e4bd32af03ef5422768f0973ecfa1d167
Mizzguy <Mizzguy@users.noreply.github.com>
2016-11-07 20:48:02 -0500

    Update Feeorin_Update.sql
    changed the mobType to 2 because im not sure why it was 3. Also do you know the exact hp of these mobs or was it another guess?

--- SQL-Updates/Feeorin_Update.sql
+++ SQL-Updates/Feeorin_Update.sql
@@ -6,17 +6,16 @@ UPDATE mob_spawn_points SET pos_x=-206, pos_y=0, pos_z=469, pos_rot=244  WHERE m
 UPDATE mob_spawn_points SET pos_x=-206.25, pos_y=0, pos_z=469, pos_rot=244  WHERE mobid=17138056;
 UPDATE mob_spawn_points SET pos_x=-206.50, pos_y=0, pos_z=469, pos_rot=244  WHERE mobid=17138057;
 
--- Feeorin pool ID : 1319
--- Fay pool ID     : 1307
---
+
+
 
 UPDATE mob_groups SET spawntype=128, dropid=0, HP=30000, MP=2000, minLevel=75, maxLevel=75 WHERE groupid=3916;
 
 UPDATE mob_groups SET spawntype=128, dropid=0, HP=15000, MP=1000, minLevel=73, maxLevel=75 WHERE groupid=3915;
 
 
---mobType , skill_list_id 195
 
-UPDATE mob_pools SET mobType=3 WHERE poolid=1307;
 
-UPDATE mob_pools SET mobType=3 WHERE poolid=1319;
+UPDATE mob_pools SET mobType=2 WHERE poolid=1307;
+
+UPDATE mob_pools SET mobType=2 WHERE poolid=1319;
